aa32c65d7b86340ac1e67567efb1b4a4e251c07e
Marc Mintel <marc@mintel.me>
2020-04-11 18:27:05 +0700

    fix test

--- packages/fs/tests/lib/read-all-json.spec.js
+++ packages/fs/tests/lib/read-all-json.spec.js
@@ -19,50 +19,50 @@ describe('readAllJson', () => {
   it('should read all json files if exists', () => {
     const mockPath = 'some/path';
     const mockFiles = ['file-1.json', 'file-2.json'];
-    const mockDocument1 = {
+    const mockJson1 = {
       title: 'test-1',
     };
-    const mockDocument2 = {
+    const mockJson2 = {
       title: 'test-2',
     };
-    const mockDocumentFiles = {
-      'file-1.json': mockDocument1,
-      'file-2.json': mockDocument2,
+    const mockJsonFiles = {
+      'some/path/file-1.json': mockJson1,
+      'some/path/file-2.json': mockJson2,
     };
 
     readDir.mockReturnValue(mockFiles);
-    readJson.mockImplementation((path) => mockDocumentFiles[path]);
+    readJson.mockImplementation((path) => mockJsonFiles[path]);
 
-    const documents = readAllJson(mockPath);
+    const jsonFiles = readAllJson(mockPath);
 
     expect(readDir).toHaveBeenCalledTimes(1);
     expect(readDir).toHaveBeenCalledWith(mockPath);
     expect(readJson).toHaveBeenCalledTimes(mockFiles.length);
-    expect(documents).toEqual([mockDocument1, mockDocument2]);
+    expect(jsonFiles).toEqual([mockJson1, mockJson2]);
   });
 
   it('should return empty array if none exists', () => {
     const mockPath = 'some/path';
     const mockFiles = [];
-    const mockDocument1 = {
+    const mockJson1 = {
       title: 'test-1',
     };
-    const mockDocument2 = {
+    const mockJson2 = {
       title: 'test-2',
     };
-    const mockDocumentFiles = {
-      'file-1.json': mockDocument1,
-      'file-2.json': mockDocument2,
+    const mockJsonFiles = {
+      'some/path/file-1.json': mockJson1,
+      'some/path/file-2.json': mockJson2,
     };
 
     readDir.mockReturnValue(mockFiles);
-    readJson.mockImplementation((path, name) => mockDocumentFiles[name]);
+    readJson.mockImplementation((path, name) => mockJsonFiles[name]);
 
-    const documents = readAllJson(mockPath);
+    const jsonFiles = readAllJson(mockPath);
 
     expect(readDir).toHaveBeenCalledTimes(1);
     expect(readDir).toHaveBeenCalledWith(mockPath);
     expect(readJson).toHaveBeenCalledTimes(0);
-    expect(documents).toEqual([]);
+    expect(jsonFiles).toEqual([]);
   });
 });
