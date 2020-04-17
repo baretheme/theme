4e0354255e13179b9ab1f7d8020002b9667d6764
Marc Mintel <marc@mintel.me>
2020-04-17 09:42:56 +0700

    chore: fix typescript tests

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,25 @@
+const { defaults } = require('jest-config');
+
 module.exports = {
+  roots: ['<rootDir>/packages'],
   collectCoverageFrom: [
-    'packages/*/**/*.{js,jsx,ts,tsx}',
+    'packages/*/src/**/*.{js,jsx,ts,tsx}',
     '!**/*.d.ts',
     '!**/node_modules/**',
   ],
+  moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/file-mock.js',
+  },
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  testPathIgnorePatterns: [...defaults.testPathIgnorePatterns, '.cache', 'dist'],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.(js|jsx|ts|tsx)$': 'ts-jest',
   },
   transformIgnorePatterns: [
     '/node_modules/',
     '^.+\\.module\\.(css|sass|scss)$',
   ],
-  moduleNameMapper: {
-    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
-  },
   snapshotSerializers: [
     'jest-emotion',
   ],
